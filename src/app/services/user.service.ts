8a25eaa7cb5c3de6adcd7905f5aca8a333769a2a
Jesus <jesuscta08@gmail.com>
2025-01-12 23:34:54 -0600

    Comunicacion entre Angular y Spring realizada con exito anadiendo un crossorigin al proyecto controller de Spring como http://localhost:4200 por defecto de Angular

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,34 +1,19 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { map, Observable, of } from 'rxjs';
 import { User } from '../models/user';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  private user: User[] = [
-    {
-      id: 1,
-      name: 'Jesus',
-      lastname: 'Moreno',
-      email: 'jesusmoreno@email.com',
-      username: 'Mmmmmmmmmmm',
-      password: '123456789',
-    },
-    {
-      id: 2,
-      name: 'Eduardo',
-      lastname: 'Larios',
-      email: 'eduardo@email.com',
-      username: 'Eeeeeeeee',
-      password: '123456789',
-    },
-  ];
+  private user: User[] = [];
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   findAll(): Observable<User[]> {
-    return of(this.user);
+    // return of(this.user);
+    return this.http.get<User[]>('http://localhost:8080/api/users');
   }
 }
 
--- users-backend/src/main/java/com/springboot/backend/jesus/usersapp/users_backend/controllers/UserController.java
+++ users-backend/src/main/java/com/springboot/backend/jesus/usersapp/users_backend/controllers/UserController.java
@@ -10,6 +10,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,7 +22,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 
 
-
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController // Marca la clase como un controlador REST que devuelve datos (JSON)
 @RequestMapping("/api/users") // Define la ruta base para este controlador
 public class UserController {
